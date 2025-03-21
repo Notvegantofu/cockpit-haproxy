7f7a51ab370745ed91554169efc43f83e0128e16
Tobias Vale <tobias.vale@protonmail.com>
2025-03-21 13:24:22 +0100

    Added simple input verification for adding

--- src/components/AddDomain.tsx
+++ src/components/AddDomain.tsx
@@ -1,10 +1,11 @@
-import { TextInput } from '@patternfly/react-core';
+import { HelperText, HelperTextItem, TextInput } from '@patternfly/react-core';
 import React, { useEffect, useState } from 'react';
 import { ProxyData } from './DomainTable';
 import { DisableSwitch } from './DisableSwitch';
 import { BackendSelect } from './BackendSelect';
 import { Td, Tr } from '@patternfly/react-table';
 import { ConfirmAdding } from './ConfirmAdding';
+import { ExclamationCircleIcon } from '@patternfly/react-icons';
 
 interface AddProps {
   proxyDataState: [ProxyData[], React.Dispatch<React.SetStateAction<ProxyData[]>>],
@@ -13,13 +14,22 @@ interface AddProps {
 
 export const AddDomain: React.FunctionComponent<AddProps> = ({ proxyDataState, backends}) => {
   const activeState = useState(true);
-  const backendState = useState("");
+  const backendState = useState("Select Backend");
   const [ domain, setDomain ] = useState("");
+  const [ invalidDomain, setInvalidDomain ] = useState(false);
+  const [ invalidBackend, setInvalidBackend ] = useState(false);
+
   function onDomainChange(_: any, newValue: string) {
     setDomain(newValue)
   }
 
-  useEffect(() => backendState[1](backends[0]), [backends[0]]); //necessary due to delay in backends content
+  useEffect(() => {
+    setInvalidDomain(false)
+  }, [domain])
+
+  useEffect(() => {
+    setInvalidBackend(false)
+  }, [backendState[0]])
 
   return (
     <Tr className='sticky-row'>
@@ -28,12 +38,26 @@ export const AddDomain: React.FunctionComponent<AddProps> = ({ proxyDataState, b
       </Td>
       <Td>
         <TextInput aria-label='DomainInput' value={domain} onChange={onDomainChange} />
+        {invalidDomain &&
+          <HelperText>
+            <HelperTextItem icon={<ExclamationCircleIcon />} variant='error'>
+              Please specify a domain
+            </HelperTextItem>
+          </HelperText>
+        }
       </Td>
       <Td>
-        <BackendSelect options={backends} backendState={backendState}/>
+        <BackendSelect options={['Select Backend'].concat(backends)} backendState={backendState} />
+        {invalidBackend &&
+          <HelperText>
+            <HelperTextItem icon={<ExclamationCircleIcon />} variant='error'>
+              Please specify a Backend
+            </HelperTextItem>
+          </HelperText>
+        }
       </Td>
       <Td textCenter>
-        <ConfirmAdding proxyDataState={proxyDataState} domainState={[domain, setDomain]} activeState={activeState} backendState={backendState} backends={backends} />
+        <ConfirmAdding proxyDataState={proxyDataState} domainState={[domain, setDomain]} activeState={activeState} backendState={backendState} setInvalidDomain={setInvalidDomain} setInvalidBackend={setInvalidBackend} />
       </Td>
       <Td />
   </Tr>
--- src/components/ConfirmAdding.tsx
+++ src/components/ConfirmAdding.tsx
@@ -3,21 +3,32 @@ import { Modal, ModalVariant, Button } from '@patternfly/react-core';
 import { ProxyData, domainmap, devMode } from './DomainTable';
 import cockpit from 'cockpit';
 import { createBackup } from './createBackup';
+import { treeRow } from '@patternfly/react-table';
 
 interface AddProps {
   proxyDataState: [ProxyData[], React.Dispatch<React.SetStateAction<ProxyData[]>>],
   domainState: [string, React.Dispatch<React.SetStateAction<string>>],
   activeState: [boolean, React.Dispatch<React.SetStateAction<boolean>>],
   backendState: [string, React.Dispatch<React.SetStateAction<string>>],
-  backends: string[]
+  setInvalidDomain: React.Dispatch<React.SetStateAction<boolean>>,
+  setInvalidBackend: React.Dispatch<React.SetStateAction<boolean>>
 }
 
-export const ConfirmAdding: React.FunctionComponent<AddProps> = ({ proxyDataState: [proxyData, setProxyData], domainState: [domain, setDomain], activeState: [active, setActive], backendState: [backend, setBackend], backends }) => {
+export const ConfirmAdding: React.FunctionComponent<AddProps> = ({ proxyDataState: [proxyData, setProxyData], domainState: [domain, setDomain], activeState: [active, setActive], backendState: [backend, setBackend], setInvalidDomain, setInvalidBackend }) => {
   const [isModalOpen, setIsModalOpen] = React.useState(false);
 
   const handleModalToggle = () => {
     setIsModalOpen(!isModalOpen);
   };
+
+  const openModal = () => {
+    if (domain === "" || backend === "Select Backend") {
+      if (domain === "") setInvalidDomain(true);
+      if (backend === "Select Backend") setInvalidBackend(true);
+      return;
+    }
+    handleModalToggle();
+  }
 
   const confirmAdding = async (_: any) => {
     const index = proxyData[proxyData.length-1].index + 1;
@@ -33,13 +44,13 @@ export const ConfirmAdding: React.FunctionComponent<AddProps> = ({ proxyDataStat
     }
     setActive(prev => true);
     setDomain(prev => "");
-    setBackend(prev => backends[0]);
+    setBackend(prev => "Select Backend");
     handleModalToggle();
   }
 
   return (
     <React.Fragment>
-      <Button variant="primary" onClick={handleModalToggle}>
+      <Button variant="primary" onClick={openModal}>
         Add
       </Button>
       <Modal
